9594feacee87bd95d05438deec0950a943b1c7a1
brinkqiang <brink.qiang@gmail.com>
2021-06-29 15:57:15 +0800

    ADD

--- tools/dmcmd4vsp/dmcmd4vsp.cpp
+++ tools/dmcmd4vsp/dmcmd4vsp.cpp
@@ -3,28 +3,51 @@
 #include "dmlog.hpp"
 #include "dmstrtk.hpp"
 #include "dmutil.h"
+#include "dmflags.h"
 
+DEFINE_string(PNAME, "dmconfigserver.exe", "process name");
+DEFINE_int64(INDEX, 1, "index=1-255");
+DEFINE_string(CMD, "start", "CMD={start,stop}");
 int main( int argc, char* argv[] )
 {
     Iexecute* execute = executeGetModule();
 
     if (NULL != execute)
     {
+        std::string strVSpath =
+            R"(D:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Team Tools\DiagnosticsHub\Collector\AgentConfigs\CPUUsageLow.json)";
+        std::string strJson = R"(CpuUsageLow.json)";
+
         std::string strPath = getenv("path");
-        std::string strCwd = DMGetWorkPath();
+        std::string strCwd = DMGetRootPath();
 
-        typedef strtk::std_string::token_deque_type match_list_type;
-        match_list_type match_list;
-        strtk::ifind_all(strCwd, strPath, std::back_inserter(match_list));
+        std::string strCmd = fmt::format(R"(tasklist | find "{}" | {})",
+                                         FLAGS_PNAME, R"(awk "{ print $2 }")");
+        std::string strRet = execute->exec(strCmd);
 
-        if (!match_list.empty())
+        if (strRet.empty())
         {
-            execute->exec("echo OK!");
-            return 0;
+            fmt::print("{} {}", "cann't find", FLAGS_PNAME);
+            return -1;
         }
 
-        std::string strCmd = fmt::format("setx /m path \"{};{}\"", strPath, strCwd);
-        execute->exec(strCmd);
+        auto PID = fmt::to_number(strRet);
+
+        std::string strVsp =
+            R"(VSDiagnostics.exe {} {} /attach:{} /loadConfig:"{}")";
+
+        std::string strVspCmd = fmt::format(strVsp, "start", FLAGS_INDEX, PID,
+                                            strVSpath);
+        std::string strVspRet = execute->exec(strVspCmd);
+
+        std::string strVspStop =
+            R"(VSDiagnostics.exe {} {} /output:{})";
+        std::string strVspStopCmd = fmt::format(strVspStop, "stop", FLAGS_INDEX,
+                                                strCwd + PATH_DELIMITER_STR + FLAGS_PNAME + "." + fmt::to_string(
+                                                        FLAGS_INDEX) + ".diagsession");
+
+        std::string strVspStopRet = execute->exec(strVspStopCmd);
+
     }
 
     return 0;
